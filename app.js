5af7ad6f3e3218cfc926817053f192d7e499b19d
Leonardo Santos Gomes <lsg@cesar.org.br>
2019-10-08 18:47:00 -0300

    lesson 30

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ const expressValidator = require("express-validator");
 const authRoutes = require("./routes/auth");
 const userRoutes = require("./routes/user");
 const categoryRoutes = require("./routes/category");
+const productRoutes = require("./routes/product");
+
 
 require("dotenv").config();
 
@@ -32,6 +34,8 @@ app.use(expressValidator());
 app.use("/api", authRoutes);
 app.use("/api", userRoutes);
 app.use("/api", categoryRoutes);
+app.use("/api", productRoutes);
+
 
 const port = process.env.PORT || 8000;
 
